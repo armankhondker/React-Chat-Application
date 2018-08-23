5cf238e77a5132ae34f623cf59abef28ab16b2d4
Arman <armankhondker@utexas.edu>
2018-08-23 16:32:23 -0500

    Updated SendMessageForm Functionality

--- src/components/SendMessageForm.js
+++ src/components/SendMessageForm.js
@@ -1,11 +1,37 @@
 import React from 'react'
 
 class SendMessageForm extends React.Component {
+    
+    constructor() {
+        super()
+        this.state = {
+            message: ''
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+    
+    handleChange(e) {
+        this.setState({
+            message: e.target.value
+        })
+    }
+    
+    handleSubmit(e) {
+        e.preventDefault()
+        console.log(this.state.message)
+        /** send off the message */
+    }
+    
     render() {
         return (
-            <form className="send-message-form">
+            <form
+                onSubmit={this.handleSubmit}
+                className="send-message-form">
                 <input
-                    placeholder="SendMessageForm"
+                    onChange={this.handleChange}
+                    value={this.state.message}
+                    placeholder="Type your message and hit ENTER"
                     type="text" />
             </form>
         )
